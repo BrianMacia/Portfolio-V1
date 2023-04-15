68ab88b0b828403de075f3e5376beadf8ca185ec
Brian Macia <brianmacia@hotmail.com>
2023-04-15 12:33:03 -0300

    1

--- src/components/Information.js
+++ src/components/Information.js
@@ -42,9 +42,7 @@ const Information = () => {
         <div>
         
           <span>Full Stack Web </span> 
-          <body>
-            <a href="http://example.com/" style="color:#FF0000;">Red Link</a>
-            </body>
+          
           <p className="myInfoCardText">Certificado de programa de desarrollo web Full Stack <a href="https://certificates.academlo.com/en/verify/60635838424675" style="color:#FF0000;" > CERTIFICAR EN WEB  </a> </p>
           
           
