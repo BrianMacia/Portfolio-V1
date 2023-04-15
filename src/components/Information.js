8552f57704a54882c21556bfbd941004dcda7010
Brian Macia <brianmacia@hotmail.com>
2023-04-15 13:22:47 -0300

    1

--- src/components/Information.js
+++ src/components/Information.js
@@ -50,7 +50,7 @@ const Information = () => {
        
           <span>Full Stack Web  </span> 
            
-          <p className="myInfoCardText">Certificado de programa de desarrollo web Full Stack <a href="https://certificates.academlo.com/en/verify/60635838424675"></a> </p>
+          <p className="myInfoCardText">Certificado de programa de desarrollo web Full Stack </p>
           
           
         </div>
